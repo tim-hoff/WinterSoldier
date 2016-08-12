488a3ad9537bb67ee9155752447d7ffffe1c1d4c
Timothy Hoff <timchoff@gmail.com>
2016-08-12 15:01:32 -0500

    Add project files.

---
+++ WinterSoldier/WinterSoldier/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Unity;
+using WinterSoldier.Views;
+
+namespace WinterSoldier
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
